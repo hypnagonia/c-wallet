7520a843f4d0c377a0ad480bd4af14213d81efb9
jenya <hypnagonia>
2023-04-20 18:41:35 -0300

    sha3 keys in storage

--- server/src/storage/index.js
+++ server/src/storage/index.js
@@ -1,14 +1,19 @@
 const { levelStorage } = require('./level/level')
+const ethers = require('ethers')
+
+const generateKey = key => ethers.keccak256(ethers.toUtf8Bytes(key.toString()))
 
 const storageFactory = async (config, logger) => {
     const s = await levelStorage(config, logger)
 
     const getWallet = async (walletId) => {
-        return s.get(walletId)
+        const key = generateKey(walletId)
+        return s.get(key)
     }
 
     const saveWallet = async (walletId, payload) => {
-        await s.put(walletId, payload)
+        const key = generateKey(walletId)
+        await s.put(key, payload)
     }
 
     const deleteAll = s.deleteAll
